d4f06b6819d1a9aadeb867f0c4766e516c90f55d
Ricardo Rocha <rocha.porto@gmail.com>
2014-04-17 10:48:05 +1200

    add call for hypervisor list.

---
+++ lib/aviator/openstack/compute/v2/admin/hypervisors.rb
@@ -0,0 +1,39 @@
+module Aviator
+
+  define_request :hypervisors, inherit: [:openstack, :common, :v2, :admin, :base] do
+
+    meta :service, :compute
+
+    link 'documentation',
+         'http://api.openstack.org/api-ref-compute-v2-ext.html#ext-os-hypervisors'
+
+    param :detail, required: false
+
+    def headers
+      super
+    end
+
+
+    def http_method
+      :get
+    end
+
+
+    def url
+      url = "#{ base_url }/os-hypervisors"
+
+      filters = []
+
+      if optional_params.include?('detail')
+        if params['detail']
+          url += "/#{detail}"
+        end
+      end
+
+      url
+    end
+
+  end
+
+end
+
